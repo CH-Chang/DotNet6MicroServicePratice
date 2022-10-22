aefce9574d7399e94e2bdd3ba33cd1ff9b8139ef
張智翔 <ch.chang.job@gmail.com>
2022-10-22 17:56:11 +0800

    feat(ProductAPI): Finish for some dto

---
+++ Mango.Services.ProductAPI/Models/Dto/ResponseDto.cs
@@ -0,0 +1,28 @@
+namespace Mango.Services.ProductAPI.Models.Dto
+{
+    /// <summary>
+    /// 回應資料轉換物件
+    /// </summary>
+    public class ResponseDto
+    {
+        /// <summary>
+        /// 是否成功
+        /// </summary>
+        public bool IsSuccess { get; set; } = false;
+
+        /// <summary>
+        /// 展示訊息
+        /// </summary>
+        public string DisplayMessage { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 錯誤訊息
+        /// </summary>
+        public IList<string> ErrorMessages { get; set; }
+
+        /// <summary>
+        /// 結果內容
+        /// </summary>
+        public object Result { get; set; }
+    }
+}
