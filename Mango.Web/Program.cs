10912c297e852c63cdaee47f8f23a10dd3cecf08
張智翔 <ch.chang.job@gmail.com>
2022-11-13 17:48:45 +0800

    feat(Web): Configure authentication

--- Mango.Web/Program.cs
+++ Mango.Web/Program.cs
@@ -8,6 +8,26 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpClient<IProductService, ProductService>();
 builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services
+    .AddAuthentication(options =>
+    {
+        options.DefaultScheme = "Cookies";
+        options.DefaultChallengeScheme = "oidc";
+    })
+    .AddCookie("Cookies", c => c.ExpireTimeSpan = TimeSpan.FromMinutes(5))
+    .AddOpenIdConnect("oidc", options =>
+    {
+        options.Authority = builder.Configuration.GetValue<string>("ServiceUrls:IdentityAPI");
+        options.GetClaimsFromUserInfoEndpoint = true;
+        options.ClientId = "mango";
+        options.ClientSecret = "secret";
+        options.ResponseType = "code";
+
+        options.TokenValidationParameters.NameClaimType = "name";
+        options.TokenValidationParameters.RoleClaimType = "role";
+        options.Scope.Add("mango");
+        options.SaveTokens = true;
+    });
 
 SD.ProductAPIBase = builder.Configuration.GetValue<string>("ServiceUrls:ProductAPI");
 
@@ -23,7 +43,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
