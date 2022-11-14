0013000c0e349990c80e6c85971388803d713b1e
張智翔 <ch.chang.job@gmail.com>
2022-11-14 17:45:00 +0800

    feat(ShoppingCartAPI): Rename for controller name

--- Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
+++ Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
@@ -5,14 +5,14 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Mango.Services.ShoppingCartAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/cart")]
     [ApiController]
-    public class CartController : Controller
+    public class CartAPIController : Controller
     {
         private readonly ICartRepository repository;
         protected ResponseDto response;
 
-        public CartController(ICartRepository repository)
+        public CartAPIController(ICartRepository repository)
         {
             this.repository = repository;
             this.response = new ResponseDto();
@@ -70,11 +70,45 @@ namespace Mango.Services.ShoppingCartAPI.Controllers
         }
 
         [HttpPost("RemoveCart")]
-        public async Task<object> UpdateCart([FromBody]int cartId)
+        public async Task<object> RemoveCart([FromBody]int cartId)
         {
             try
             {
                 bool isSuccess = await repository.RemoveFromCart(cartId);
+                response.Result = isSuccess;
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+
+            return response;
+        }
+
+        [HttpPost("ApplyCoupon")]
+        public async Task<object> ApplyCoupon([FromBody] CartDto cartDto)
+        {
+            try
+            {
+                bool isSuccess = await repository.ApplyCoupon(cartDto.CartHeader.UserId, cartDto.CartHeader.CouponCode);
+                response.Result = isSuccess;
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+
+            return response;
+        }
+
+        [HttpPost("RemoveCoupon")]
+        public async Task<object> RemoveCoupon([FromBody] string userId)
+        {
+            try
+            {
+                bool isSuccess = await repository.RemoveCoupon(userId);
                 response.Result = isSuccess;
             }
             catch (Exception ex)
