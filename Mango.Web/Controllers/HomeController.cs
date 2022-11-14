c8c027adb221d2e6e9ad5ce162b56cb82432ba17
張智翔 <ch.chang.job@gmail.com>
2022-11-14 13:26:24 +0800

    feat(Web): Archieve get all product for home index

--- Mango.Web/Controllers/HomeController.cs
+++ Mango.Web/Controllers/HomeController.cs
@@ -1,45 +1,57 @@
-using Mango.Web.Models;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
-
 namespace Mango.Web.Controllers
 {
+    using System.Diagnostics;
+    using Mango.Web.Models;
+    using Mango.Web.Services.IServices;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using Newtonsoft.Json;
+
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ILogger<HomeController> logger;
+        private readonly IProductService productService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IProductService productService)
         {
-            _logger = logger;
+            this.logger = logger;
+            this.productService = productService;
+
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<ProductDto> list = new ();
+            var response = await this.productService.GetAllProductsAsync<ResponseDto>(string.Empty);
+            if (response != null && response.IsSuccess)
+            {
+                string result = Convert.ToString(response.Result) ?? throw new ArgumentException("Result should not be null");
+                list = JsonConvert.DeserializeObject<List<ProductDto>>(result) ?? throw new ArgumentException("Json string should not be \"null\"");
+            }
+
+            return this.View(list);
         }
 
         public IActionResult Privacy()
         {
-            return View();
+            return this.View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return this.View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
         }
 
         [Authorize]
         public async Task<IActionResult> Login()
         {
-            return RedirectToAction(nameof(Index));
+            return this.RedirectToAction(nameof(this.Index));
         }
 
         public IActionResult Logout()
         {
-            return SignOut("Cookies", "oidc");
+            return this.SignOut("Cookies", "oidc");
         }
     }
 }
