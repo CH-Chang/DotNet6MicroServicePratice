8b989391d659f6374d271f753aacc5000ebd469e
張智翔 <ch.chang.job@gmail.com>
2022-11-14 15:09:30 +0800

    feat(Web): Add cart dto

---
+++ Mango.Web/Models/CartHeaderDto.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Web.Models
+{
+    public class CartHeaderDto
+    {
+        public int CartHeaderId { get; set; }
+
+        public string UserId { get; set; }
+
+        public string CouponCode { get; set; }
+
+        public double OrderTotal { get; set; }
+    }
+}
