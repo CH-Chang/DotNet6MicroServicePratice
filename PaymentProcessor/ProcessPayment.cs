5861716fa21f257242ceaa36db9f48dd769b8a7a
張智翔 <ch.chang.job@gmail.com>
2022-11-26 12:47:10 +0800

    feat(PaymentProcessor): Add mock payment processor

---
+++ PaymentProcessor/ProcessPayment.cs
@@ -0,0 +1,10 @@
+namespace PaymentProcessor
+{
+    public class ProcessPayment : IProcessPayment
+    {
+        public bool PaymentProcessor()
+        {
+            return true;
+        }
+    }
+}
